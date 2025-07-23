bdc45949bad365e3eab0ae5745543c937a7f71f1
pipe7777 <ocampofelipe87@gmail.com>
2025-07-22 20:54:49 -0500

    actualizacion de la tabla listar

--- index.php
+++ index.php
@@ -124,9 +124,103 @@
     </div>
   </div>
 </div>
+<br>
+<br>
 
     
    <script src ="js/modal.js"></script>
+
+
+
+
+
+
+
+   <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+      <?php
+      include_once "Controller/Conexion.php";
+      $conexion = new Conexion();
+      $conexion = $conexion->conectar();
+      if ($conexion) {
+        $sql = "SELECT * FROM registropersonas";
+        $consulta=$conexion->prepare($sql);
+        $consulta->execute();
+        $i = 0;
+        while($fila=$consulta-> fetch(PDO:: FETCH_ASSOC)){
+          $i += 1;
+        
+      ?>
+        
+<table class="table table-dark table-striped">
+  
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Correo</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Editar</th>
+      <th scope="col">Eliminar</th>
+
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+
+
+    </tr>
+
+   
+  </tbody>
+<?php
+}}
+else {
+  echo "No exixte la conexion";
+}
+?>
+
+
+
+
+</table>
+
+
+
+      
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
